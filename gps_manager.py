03b95c056ad299a2c529e0aebe2a274de6a2ad66
Aaron Kovacs <aaronk4576@gmail.com>
2019-12-06 11:28:28 -0500

    Added gps_manager

---
+++ gps_manager.py
@@ -0,0 +1,19 @@
+import sh
+from sh import git
+import time
+import os, sys
+import subprocess as sp
+
+extProc = None 
+
+if __name__ == "__main__":
+
+    if extProc is None:
+        print("*********** Started GPS **************")                                                     
+        extProc = sp.Popen(['python', 'gps.py'])
+
+    if sp.Popen.poll(extProc) is not None:
+        print("*********** Error! GPS Restarted **************")                                                     
+        extProc = sp.Popen(['python', 'gps.py'])
+                                                                                                                   
+    time.sleep(10) # Check every 30 seconds
