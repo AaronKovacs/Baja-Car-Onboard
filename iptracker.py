d4a01b947fc7f38144cee92f67e701bb1890146e
Aaron Kovacs <aaronk4576@gmail.com>
2020-01-21 18:47:42 -0500

    Added pi management scripts

---
+++ iptracker.py
@@ -0,0 +1,23 @@
+import requests
+import io
+import socket
+import os
+
+while True:
+    try: 
+        gw = os.popen("ip -4 route show default").read().split()
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect((gw[2], 0))
+        host_ip = s.getsockname()[0]
+        gateway = gw[2]
+        host_name = socket.gethostname()
+
+        value_str = "[Baja-Car] Host: %s IP: %s" % (host_name, host_ip)
+        try:
+            requests.post('http://prod.ft7mz3prg3.us-east-1.elasticbeanstalk.com/misc/text', json={'text': value_str})
+            print("Sent IP Address")
+        except:
+            print('Couldn\'t POST data to remote. Throwing out text...')
+        except: 
+            print("Unable to get Hostname and IP") 
+    time.sleep(10)
---
+++ network-monitor.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+command > /dev/null 2>&1
+while true ; do
+    if ifconfig wlan0 | grep -q "inet addr:" ; then
+        sleep 60
+    else
+        ifup --force wlan0
+        sleep 10
+    fi
+done
