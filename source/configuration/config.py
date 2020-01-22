33ee2c986a0a77ccd25b4f7a5b71987a034f103a
Aaron Kovacs <aaronk4576@gmail.com>
2020-01-21 19:53:32 -0500

    Update config.py

--- source/configuration/config.py
+++ source/configuration/config.py
@@ -4,7 +4,7 @@ import os
 PAGE_SIZE = 20
 
 # Database
-PROD_DB = 'sqlite:///home/pi/database.db'
+PROD_DB = '/home/pi/database.db'
 
 REMOTE = 'http://prod-env.bb389wsu7v.us-east-1.elasticbeanstalk.com'
 
